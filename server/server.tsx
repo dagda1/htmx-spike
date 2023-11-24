8be47e4ed7c1c459fe38b1cb360f818c9ce0ff69
Paul Cowan <paul.cowan@cutting.scot>
2023-11-24 15:41:24 +0000

    use hono

---
+++ server/server.tsx
@@ -0,0 +1,34 @@
+import { Hono } from 'hono';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
+import { AddSomething, Item, renderer } from './components';
+
+const app = new Hono();
+
+app.get('*', renderer);
+
+app.get('/', (c) => {
+  return c.render(
+    <div>
+      <AddSomething />
+      <div id="something"></div>
+    </div>,
+  );
+});
+
+app.post(
+  '/something',
+  zValidator(
+    'form',
+    z.object({
+      title: z.string().min(1),
+    }),
+  ),
+  async (c) => {
+    const { title } = c.req.valid('form');
+    const id = crypto.randomUUID();
+    return c.html(<Item title={title} id={id} />);
+  },
+);
+
+export default app;
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
-import inject from '@rollup/plugin-inject';
 import { defineConfig } from 'vite';
+import devServer from '@hono/vite-dev-server';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4400/',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace('/api', ''),
-        headers: {
-          
-        }
-      },
-    },
+  esbuild: {
+    loader: 'tsx',
+    include: ['server/**/*.tsx'],
   },
   plugins: [
-    inject({
-      htmx: 'htmx.org',
+    devServer({
+      entry: 'server/server.tsx',
+      exclude: ['src'],
     }),
   ],
 });
