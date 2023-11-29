0b69ff650495df75642ac504480dc88a40fb5e97
Paul Cowan <paul.cowan@cutting.scot>
2023-11-29 16:24:00 +0000

    only return AppShell with no hx headers

--- app/handlers/get-events.tsx
+++ app/handlers/get-events.tsx
@@ -5,10 +5,14 @@ import type { HandlerContext } from '@/types';
 import { AppShell } from '@/Layouts/AppShell';
 
 export async function getEvents(c: HandlerContext): Promise<Response> {
-  // const trigger = c.req.header('HX-Trigger');
+  const trigger = c.req.header('HX-Trigger');
   const requestedDate = parseShortDate(c.req.query('date') ?? '') ?? new Date();
   const startDate = DateTime.fromJSDate(requestedDate).startOf('week');
   // const endDate = startDate.plus({ weeks: 1 });
+
+  if (trigger) {
+    return c.html(<WeekCalendar startDate={startDate.toJSDate()} events={[]} />);
+  }
 
   return c.html(
     <AppShell>
